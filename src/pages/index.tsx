75fdfd6357b85727e0eff6d17b364ab120daecf5
Matheus-FSantos <matheus.fs.contato@gmail.com>
2023-12-21 13:59:31 -0300

    feat(home): add page body

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ const Home = () => {
 				<main className={ styles.contentContainer }>
 					<section className={ styles.hero }>
 						<span>👏 Hey, welcome</span>
-						<h1>News about <br /> the <span>React</span> world.</h1>
+						<h1>News about <br /> th <span>React</span> world.</h1>
 						<p>
 							Get access to all the publications <br />
 							<span>for $9.90 month</span>
