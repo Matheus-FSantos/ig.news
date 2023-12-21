5f8898abec504dd5540536441ecda93ed486995f
Matheus-FSantos <matheus.fs.contato@gmail.com>
2023-12-21 15:31:30 -0300

    feat(payment): add stripe req to get product price on stripe site and insert 'GetStaticProps' to make a static html of next website

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,22 @@ import styles from "./home.module.scss";
 import { Header } from "../components/Header";
 import { SubscribeButton } from "@/components/SubscribeButton";
 import { ReactElement } from "react";
+import { GetStaticProps } from "next";
+import { stripe } from "../services/stripe";
 
-const Home = (): ReactElement => {
+interface IHomeProps {
+	product: {
+		priceId: string,
+		amount: number
+	}
+}
+
+interface IProduct {
+	priceId: string,
+	amount: string
+}
+
+const Home = ({ product }: IHomeProps): ReactElement => {
   return (
 		<>
 			<Head>
@@ -18,9 +32,11 @@ const Home = (): ReactElement => {
 						<h1>News about <br /> the <span>React</span> world.</h1>
 						<p>
 							Get access to all the publications <br />
-							<span>for $9.90/month</span>
+							<span>for {product.amount}/month</span>
 						</p>
-						<SubscribeButton />
+						<SubscribeButton
+							priceId={ product.priceId }
+						/>
 					</section>
 					
 					<img src="/images/woman.svg" alt="Girl coding" />
@@ -28,6 +44,29 @@ const Home = (): ReactElement => {
 			</div>
 		</>
   )
+}
+
+const getStaticProps: GetStaticProps = async () => {
+	const price = await stripe.prices.retrieve("price_1OPppfAevb283gVVqn7hoXTN");
+
+	const product: IProduct = {
+		priceId: price.id,
+		amount: new Intl.NumberFormat('en', {
+			style: "currency",
+			currency: "USD",
+		}).format(Number(price.unit_amount) / 100),
+	}
+
+	return {
+		props: {
+			product
+		},
+		revalidate: 60 * 60 * 24 /* 24 hours */
+	}
+}
+
+export {
+	getStaticProps
 }
 
 export default Home;
