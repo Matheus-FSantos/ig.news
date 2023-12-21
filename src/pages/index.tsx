0be26510918573e4852e8c3c15fb4ac27dbb4043
Matheus-FSantos <matheus.fs.contato@gmail.com>
2023-12-21 13:58:15 -0300

    feat

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,27 @@
 import Head from "next/head";
+import styles from "./home.module.scss";
 import { Header } from "../components/Header";
 
 const Home = () => {
   return (
 		<>
 			<Head>
-				<title>Início | ig.news</title>
+				<title>Home | ig.news</title>
 			</Head>
 			<div>
 				<Header/>
-				<h1>Hello World</h1>
+				<main className={ styles.contentContainer }>
+					<section className={ styles.hero }>
+						<span>👏 Hey, welcome</span>
+						<h1>News about <br /> the <span>React</span> world.</h1>
+						<p>
+							Get access to all the publications <br />
+							<span>for $9.90 month</span>
+						</p>
+					</section>
+					
+					<img src="/images/woman.svg" alt="Girl coding" />
+				</main>
 			</div>
 		</>
   )
