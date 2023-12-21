2d09be55f2693a66c24947c05119c971059c203f
Matheus-FSantos <matheus.fs.contato@gmail.com>
2023-12-21 13:43:09 -0300

    feat(sigin): add conditional rendering

--- src/components/SignInButton /index.tsx
+++ src/components/SignInButton /index.tsx
@@ -1,11 +1,33 @@
 import { ReactElement } from "react";
+import { FaGithub } from "react-icons/fa";
+import { FiX } from "react-icons/fi";
+import styles from "./styles.module.scss";
 
 const SignInButton = (): ReactElement => {
-	return(
-		<div>
-
-		</div>
-	);
+	
+	const isUserLoggedIn = true;
+	
+	return isUserLoggedIn ? 
+		(
+			<button
+				type="button"
+				className={ styles.signInButton }
+			>
+				<FaGithub color="#04d361" />
+				Matheus-FSantos
+				<FiX color="#737380" className={ styles.closeIcon } />
+			</button>
+		)
+	:
+		(
+			<button
+				type="button"
+				className={ styles.signInButton }
+			>
+				<FaGithub color="#eba417" />
+				Sign in w. Github
+			</button>
+		)
 }
 
 export { SignInButton };
