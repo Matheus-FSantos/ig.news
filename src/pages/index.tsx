d25c4f4a45b2243141ad7153f72e4687615c9b69
Matheus-FSantos <matheus.fs.contato@gmail.com>
2023-12-21 14:25:42 -0300

    refactor: refactor on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ const Home = () => {
 						<h1>News about <br /> the <span>React</span> world.</h1>
 						<p>
 							Get access to all the publications <br />
-							<span>for $9.90 month</span>
+							<span>for $9.90/month</span>
 						</p>
 						<SubscribeButton />
 					</section>
